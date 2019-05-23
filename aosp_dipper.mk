49e852fbb060051f3227c655b09f6b7ece1943c7
Henrique Pereira <pereirah2001@gmail.com>
2019-05-22 21:32:14 -0300

    dipper: Include ARCore
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- aosp_dipper.mk
+++ aosp_dipper.mk
@@ -10,6 +10,7 @@ $(call inherit-product, device/xiaomi/dipper/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_dipper
