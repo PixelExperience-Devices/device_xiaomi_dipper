64f1a7adb3131a4d87146823f2f9d1c6c4600306
argraur <argraur@gmail.com>
2019-09-07 07:31:05 +0000

    dipper: move gps parameters to gps_debug.conf
    * config_gpsParameters gone in Android10
    
    Signed-off-by: argraur <argraur@gmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps_debug.conf:system/etc/gps_debug.conf
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
