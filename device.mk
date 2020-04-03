df87cd7ecadda886358ec97fd2c0885c45efe44e
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-04-03 12:40:54 -0300

    dipper: Update firmware folder path

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/dipper/dipper-vendor.mk)
 
 # Firmware
-$(call inherit-product, vendor/xiaomi/firmware/dipper/firmware.mk)
+$(call inherit-product, vendor/xiaomi-firmware/dipper/firmware.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2248
