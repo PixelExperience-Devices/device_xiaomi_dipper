ace71763cb107a22087f53a37dda5573d695eaa3
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-07-05 14:27:31 -0300

    dipper: Let's use the same kernel as polaris
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ TARGET_SCREEN_DENSITY := 411
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/dipper
 TARGET_KERNEL_CONFIG := dipper_defconfig
+TARGET_KERNEL_CLANG_VERSION := proton
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
