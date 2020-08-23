bfeee93676dfadf9dd328d48324a309b6b64bcdf
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-08-23 14:41:14 -0300

    Revert "dipper: Enable TARGET_INCLUDE_WIFI_EXT"
    This reverts commit bdfe4309140072ea570ccacadf1c8d5b27b99b5b.
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- aosp_dipper.mk
+++ aosp_dipper.mk
@@ -8,7 +8,6 @@ $(call inherit-product, device/xiaomi/dipper/device.mk)
 
 # Inherit some common PixelExperience stuff.
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
