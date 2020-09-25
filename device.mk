a23dcc739d9f553bde15a86d1fd606be81a45a4d
Chenyang Zhong <zhongcy95@gmail.com>
2020-09-24 21:08:48 -0400

    dipper: use AOSP nfc@1.2 for now
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- device.mk
+++ device.mk
@@ -90,20 +90,13 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    com.gsma.services.nfc \
-    com.nxp.nfc.nq \
-    libnqnfc_nci_jni \
-    nfc_nci.nqx.default.hw \
-    NQNfcNci \
-    nqnfcee_access.xml \
-    nqnfcse_access.xml \
+    android.hardware.nfc@1.2-service \
+    NfcNci \
     Tag \
-    vendor.nxp.hardware.nfc@2.0-service
+    android.hardware.secure_element@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # NQ Client
