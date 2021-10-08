a69a5b47e4707f545c32465a6fd415f5dfc16c60
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-08 09:48:37 -0400

    dipper: ship cgroups.json and task_profiles.json in vendor
    Google's prebuilt cgroups.json and task_profiles.json for products
    launched with previous API levels only covers
    ro.product.first_api_level >= 28. [1]
    
    Polaris was launched with Oreo (API level 26), so schedtune and
    task groups are completely broken. Since the system also checks
    /vendor/etc for vendor profiles, make a copy of cgroups_28.json
    and task_profiles_28.json and ship them to /vendor/etc. Profiles
    for previous API levels are all the same anyway.
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -58,6 +58,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Device fstab
 PRODUCT_PACKAGES += \
     fstab.qcom
