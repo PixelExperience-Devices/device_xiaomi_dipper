1275723cab65b05dc4567360ed73262af94d281f
LucyfMorning <unreadmatheus@gmail.com>
2022-01-15 19:16:48 +0000

    merge support to aosp recovery and quick tap all commited by GiaSen <giammasena@gmail.com> with tags: c0703b4d60d308eac127b35641a8dac175e67678 and 1632c7e4b31f69bdaed63fd2f10cbb9faf382cbf

--- evolution_dipper.mk
+++ evolution_dipper.mk
@@ -9,7 +9,8 @@ $(call inherit-product, device/xiaomi/dipper/device.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
 
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
