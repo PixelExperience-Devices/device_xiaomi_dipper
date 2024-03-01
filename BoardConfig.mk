d6a1624ea53b0bcac99a415514193c5a29c9aa35
giasen <giammasena@gmail.com>
2024-03-01 22:25:51 +0100

    dipper: switch to real common kernel
    same as older

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ TARGET_USES_COLOR_METADATA := true
 TARGET_SCREEN_DENSITY := 411
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/xiaomi/dipper
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 TARGET_KERNEL_CONFIG := dipper_defconfig
 TARGET_KERNEL_CLANG_VERSION := prelude
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin
